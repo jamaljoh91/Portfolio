30b88b36161812cfdc05d6996303ef2fa1dbaa88
Vonta Johnson <devontajohnsonit@gmail.com>
2017-08-20 19:00:20 -0400

    scroll effect added

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+//ScrollFunction
+$('li').click(function(event) {
+  
+  event.preventDefault();
+  const section = $(this).text().toLowerCase();
+  const info = $(this);
+
+  console.log(section);
+  console.log(info);
+  
+  $.scrollTo(document.getElementById(section), 800, {
+        offset:-60,
+        onAfter: function() {
+          requestAnimationFrame(function() {
+              $(info).addClass('active').siblings().removeClass('active');
+          });
+        }
+      });
+  });
+});
 
 const revealSettings = {
   reset:true,
@@ -7,5 +28,5 @@ const revealSettings = {
 };
 
 window.sr = ScrollReveal(revealSettings);
-sr.reveal('.about', {viewFactor:0.4});
+sr.reveal('#about', {viewFactor:0.4});
 sr.reveal('.sr', {duration: 750}, 250);
