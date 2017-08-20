c411e36b689d5fb9ee0485c7ff8dd415ec01eaa3
Vonta Johnson <devontajohnsonit@gmail.com>
2017-08-20 16:15:36 -0400

    scroll effect added

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,11 @@
 
-// const revealSettings = {
-//   reset:true,
-//   origin: 'right'
-// };
+const revealSettings = {
+  reset:true,
+  duration: 500,
+  origin: 'right',
+  viewFactor:0.3,
+};
 
-// window.sr = ScrollReveal(revealSettings);
-// sr.reveal('.about');
-// sr.reveal('.projects');
+window.sr = ScrollReveal(revealSettings);
+sr.reveal('.about', {viewFactor:0.4});
+sr.reveal('.sr', {duration: 750}, 250);
