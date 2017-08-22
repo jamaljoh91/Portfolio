bf17d5494b3ef670ba28f1433b6299f602545cf3
Vonta Johnson <devontajohnsonit@gmail.com>
2017-08-21 21:47:09 -0400

    syntax

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-//ScrollFunction
-$('li').click(function(event) {
-  
-  event.preventDefault();
-  const section = $(this).text().toLowerCase();
-  const info = $(this);
-  
-  $.scrollTo(document.getElementById(section), 800);
-  
-  });
+  //ScrollFunction
+  $('li').click(function(event) {
+    
+    event.preventDefault();
+    const section = $(this).text().toLowerCase();
+    const info = $(this);
+    
+    $.scrollTo(document.getElementById(section), 800);
+    
+    });
 
-//Nav opener 
-$(function() {
-  $('.toggleNav').on('click',function() {
-    $('nav ul').toggleClass('open');
+  //Nav opener 
+  $(function() {
+    $('.toggleNav').on('click',function() {
+      $('nav ul').toggleClass('open');
+    });
   });
-});
 
 });
 
