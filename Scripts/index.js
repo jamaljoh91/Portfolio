7cebac5ea27d4aeb85a2790f1aa96eeeb130d531
Vonta Johnson <devontajohnsonit@gmail.com>
2018-10-12 22:30:42 -0400

    get layout back to working

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,32 +1,19 @@
 $(document).ready(function() {
   //ScrollFunction
-  $('li').click(function(event) {
-    
+  $("li").click(function(event) {
     event.preventDefault();
-    const section = $(this).text().toLowerCase();
+    const section = $(this)
+      .text()
+      .toLowerCase();
     const info = $(this);
-    
-    $.scrollTo(document.getElementById(section), 800);
-    
-    });
 
-  //Nav opener 
-  $(function() {
-    $('.toggleNav').on('click',function() {
-      $('nav ul').toggleClass('open');
-    });
+    $.scrollTo(document.getElementById(section), 800);
   });
 
+  //Nav opener
+  $(function() {
+    $(".toggleNav").on("click", function() {
+      $("nav ul").toggleClass("open");
+    });
+  });
 });
-
-const revealSettings = {
-  reset:true,
-  duration: 500,
-  origin: 'right',
-  viewFactor:0.3,
-  mobile:false
-};
-
-window.sr = ScrollReveal(revealSettings);
-sr.reveal('#about', {viewFactor:0.4});
-sr.reveal('.sr', {duration: 750}, 250);
